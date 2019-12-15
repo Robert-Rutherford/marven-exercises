b44f880ad9461ae5156c9ebca9c5d0140d5ba4bf
Robert Rutherford <robertl.rutherford2@gmail.com>
2019-12-15 14:57:26 -0600

    exercise

--- src/main/java/DependencyManagementExercise.java
+++ src/main/java/DependencyManagementExercise.java
@@ -9,16 +9,16 @@ public class DependencyManagementExercise {
         System.out.println("enter Something:");
         String input = scan.nextLine();
         System.out.println();
-        System.out.printf("You entered: %s%n",input);
+        System.out.printf("You entered: %s%n", input);
 
-        if (StringUtils.isNumeric(input)){
-            System.out.printf("\"%s\" is a number%n",input);
-        }else {
-            System.out.printf("\"%s\" is not a number%n",input);
+        if (StringUtils.isNumeric(input)) {
+            System.out.printf("\"%s\" is a number%n", input);
+        } else {
+            System.out.printf("\"%s\" is not a number%n", input);
         }
 
-        System.out.printf("Flipped case: \"%s\"%n",StringUtils.swapCase(input));
-        System.out.printf("Reversed: \"%s\"%n",StringUtils.reverse(input));
+        System.out.printf("Flipped case: \"%s\"%n", StringUtils.swapCase(input));
+        System.out.printf("Reversed: \"%s\"%n", StringUtils.reverse(input));
 
     }
 }
